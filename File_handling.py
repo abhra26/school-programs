f502a7bef2b06b690d25ed09d3e2b2f2eedeb2ad
Abhraneel Saha <abhraneel2003@gmail.com>
2020-06-23 21:27:39 +0530

    1st assignment

--- File_handling.py
+++ File_handling.py
@@ -1,20 +1,18 @@
 # 1. Write a function to open the text file ABC.txt and count the number of words that start with ‘the’, like: the, their, they, them, these etc.
 
+def read_it(file):
+    fl = open(file,"r")
+    count = 0
+    lis = ['the', 'The', 'THE']
+    for line in fl.readlines():
+        try:
+            for word in line.rstrip().split():
+                if word[:3] in lis:
+                    count += 1
+        except:
+            pass
+    fl.close()
+    return count
 
-fl = open("ABC","r")
-count = 0
-lis = ['the', 'The', 'THE']
-for line in fl.readlines():
-    try:
-        for word in line.rstrip().split():
-            if word[:3] in lis:
-                count += 1
-    except:
-        pass
-
-
-
-fl.close()
-
-print(f'no. of words starting with the : {count}')
+print(f'no. of words starting with the : {read_it("ABC")}')
 
