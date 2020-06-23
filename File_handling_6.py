36eceac1fda8549fb8a486a7b61505dac6f22b85
Abhraneel Saha <abhraneel2003@gmail.com>
2020-06-23 10:14:19 +0530

    1st assignment

--- File_handling_6.py
+++ File_handling_6.py
@@ -4,7 +4,7 @@ def copy(file1,file2):
     with open(file1) as fl1, open(file2,"w") as fl2:
         for lines in fl1.readlines():
             try:
-                if lines.rstrip()[0].isupper() == False:
+                if lines.rstrip()[0].isupper():
                     fl2.write(lines)
 
             except:
