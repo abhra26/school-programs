2ae242cf6d68ce34aa47ece6b4a456d5f8b0feac
Abhraneel Saha <abhraneel2003@gmail.com>
2020-07-04 12:15:35 +0530

    1st assignment

---
+++ bin_1.py
@@ -0,0 +1,14 @@
+# 1. Program to append or add employee details(Empno, name, address, salary) to binary file
+
+def add_to(file, data):
+    import pickle
+    with open(file,"ab") as fl:
+        for i in range(data):
+            Empno = (input("Empno:"))
+            name = input("name:")
+            address = input("address:")
+            salary = (input("salary:"))
+            ls = Empno + ' ' + name + ' ' + address + ' ' + salary + '\n'
+            pickle.dump(ls, fl)
+
+add_to("bin_1_file",2)
